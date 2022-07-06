973b581abdef186cefb48f31cc6817c97213b621
Nima Aghaei <88058268+nimaMamAghaei@users.noreply.github.com>
2022-07-06 15:30:06 +0430

    Create Page and Page Model for Update Profile

---
+++ Server/Pages/Account/UpdateProfile.cshtml.cs
@@ -0,0 +1,165 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Infrastructure.Extensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Server.Pages.Account
+{
+    /// <summary>
+    /// صفحه ویرایش اطلاعات کاربر توسط کاربر
+    /// نیاز به لاگین بودن در سایت
+    /// </summary>
+    [Microsoft.AspNetCore.Authorization.Authorize]
+    public class UpdateModel : Infrastructure.BasePageModelWithDatabase
+    {
+        public UpdateModel
+            (Persistence.DatabaseContext databaseContext) : base(databaseContext: databaseContext)
+        {
+            ViewModel = new();
+        }
+
+        [Microsoft.AspNetCore.Mvc.BindProperty]
+        public ViewModels.Pages.Account.Users.UpdateProfileViewModel ViewModel { get; set; }
+
+        /// <summary>
+        /// دریافت اطلاعات کاربر داخل دیتابیس بر اساس نام کاربری آن
+        /// و نمایش آن در فرم ویرایش اطلاعات
+        /// </summary>
+        /// <returns></returns>
+        public
+            async System.Threading.Tasks.Task OnGetAsync
+            (System.Threading.CancellationToken cancellationToken)
+        {
+            ViewData["CultureName"] = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+            
+            var userName = HttpContext.User.FindFirstValue
+                (System.Security.Claims.ClaimTypes.Name); //Get UserName
+            
+            if (DatabaseContext is not null)
+            {
+                var foundedUser = await DatabaseContext
+                    .Users
+                    .FirstOrDefaultAsync(user => user.Username == userName, cancellationToken);
+                if (foundedUser is not null)
+                {
+                    ViewModel.Id = foundedUser.Id;
+                    ViewModel.Username = foundedUser.Username;
+                    ViewModel.Gender = foundedUser.Gender;
+                    ViewModel.FirstName = foundedUser.FirstName;
+                    ViewModel.LastName = foundedUser.LastName;
+                    ViewModel.NationalCode = foundedUser.NationalCode;
+                    ViewModel.Description = foundedUser.Description;
+                    if (ViewData["CultureName"].ToString() == "fa-IR")
+                    {
+                        ViewModel.PersianBirthDate = foundedUser.BirthDate?.ToPersianDate();
+                    }
+                    else
+                    {
+                        ViewModel.EnglishBirthDate = foundedUser.BirthDate;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// ثبت اطلاعات ویرایش شده توسط کاربر در دیتابیس
+        /// </summary>
+        /// <returns></returns>
+        public 
+            async System.Threading.Tasks.Task<Microsoft.AspNetCore.Mvc.IActionResult> OnPostAsync
+            (System.Threading.CancellationToken cancellationToken)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            try
+            {
+                var currentCultureName = System.Threading.Thread.CurrentThread.CurrentUICulture.Name;
+
+                if (DatabaseContext is not null)
+                {
+                    // **************************************************
+                    var foundedUser = await DatabaseContext
+                        .Users
+                        .FirstOrDefaultAsync(user => user.Id == ViewModel.Id, cancellationToken);
+                    // **************************************************
+
+                    // **************************************************
+                    string? fixedUsername = Infrastructure.Utility
+                        .RemoveSpacesAndMakeTextCaseInsensitive(text: ViewModel.Username);
+                    //if Changed UserName
+                    if (foundedUser.Username != fixedUsername)
+                    {
+                        bool isUsernameFound = await DatabaseContext
+                            .Users
+                            .Where(current => current.Username == fixedUsername)
+                            .AnyAsync(cancellationToken);
+                        //if Exist Username in database
+                        if (isUsernameFound)
+                        {
+                            string alreadyExistsUserName = string.Format
+                                (format: Resources.Messages.Errors.AlreadyExists,arg0: Resources.DataDictionary.Username);
+
+                            AddPageError(message: alreadyExistsUserName);
+                            return Page();
+                        }
+                    }
+                    // **************************************************
+
+                    // **************************************************
+                    if (foundedUser is not null)
+                    {
+                        foundedUser.Username = ViewModel.Username;
+                        foundedUser.Gender = ViewModel.Gender;
+                        foundedUser.FirstName = ViewModel.FirstName;
+                        foundedUser.LastName = ViewModel.LastName;
+                        foundedUser.NationalCode = ViewModel.NationalCode;
+                        foundedUser.Description = ViewModel.Description;
+                        if (currentCultureName == "fa-IR")
+                        {
+                            foundedUser.BirthDate = ViewModel.PersianBirthDate?.ToPersianDate();
+                        }
+                        else
+                        {
+                            foundedUser.BirthDate = ViewModel.EnglishBirthDate;
+                        }
+
+
+                        DatabaseContext.Users.Update(foundedUser);
+                        await DatabaseContext.SaveChangesAsync(cancellationToken);
+
+                        // **************************************************
+                        var successMessage = string.Format
+                            (format: Resources.Messages.Successes.SuccessUpdate, arg0: Resources.DataDictionary.User);
+                        var toastSuccess = AddToastSuccess(message: successMessage);
+                        return RedirectToPage("/Account/Profile", toastSuccess);
+                        // **************************************************
+                    }
+                    // **************************************************
+                }
+
+                var toastError = AddToastError(message: Resources.Messages.Errors.UnexpectedError);
+                return RedirectToPage("/Account/Profile", toastError);
+            }
+            catch (System.Exception exception)
+            {
+                System.Console.WriteLine(value: exception.Message);
+                
+                var toastError = AddToastError(message: Resources.Messages.Errors.UnexpectedError);
+                return RedirectToPage("/Account/Profile", toastError);
+            }
+            finally
+            {
+                if (DatabaseContext is not null)
+                {
+                    await DatabaseContext.DisposeAsync();
+                    DatabaseContext = null;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ ViewModels/Pages/Account/Users/UpdateProfileViewModel.cs
@@ -0,0 +1,111 @@
+﻿namespace ViewModels.Pages.Account.Users
+{
+    public class UpdateProfileViewModel : object
+    {
+        public UpdateProfileViewModel() : base()
+        {
+
+        }
+
+        // **********
+        [System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.Id),
+            ResourceType = typeof(Resources.DataDictionary))]
+        public System.Guid Id { get; set; }
+        // **********
+
+        // **********
+        [System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.Username),
+            ResourceType = typeof(Resources.DataDictionary))]
+        
+        [System.ComponentModel.DataAnnotations.Required(
+            AllowEmptyStrings = false,
+            ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+            ErrorMessageResourceName = nameof(Resources.Messages.Validations.Required))]
+        
+        [System.ComponentModel.DataAnnotations.MaxLength
+        (length: Domain.SeedWork.Constant.MaxLength.Username,
+            ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+            ErrorMessageResourceName = nameof(Resources.Messages.Validations.MaxLength))]
+        
+        [System.ComponentModel.DataAnnotations.RegularExpression
+        (pattern: Domain.SeedWork.Constant.RegularExpression.Username,
+            ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+            ErrorMessageResourceName = nameof(Resources.Messages.Validations.Username))]
+        public string? Username { get; set; }
+        // **********
+
+        // **********
+        [System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.Gender),
+            ResourceType = typeof(Resources.DataDictionary))]
+        public Domain.Account.Enumerations.Gender Gender { get; set; }
+        // **********
+
+        // **********
+        [System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.FirstName),
+            ResourceType = typeof(Resources.DataDictionary))]
+
+        [System.ComponentModel.DataAnnotations.MaxLength(
+            length: Domain.SeedWork.Constant.MaxLength.FirstName,
+            ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+            ErrorMessageResourceName = nameof(Resources.Messages.Validations.MaxLength))]
+        public string? FirstName { get; set; }
+        // **********
+
+        // **********
+        [System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.LastName),
+            ResourceType = typeof(Resources.DataDictionary))]
+
+        [System.ComponentModel.DataAnnotations.MaxLength(
+            length: Domain.SeedWork.Constant.MaxLength.LastName,
+            ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+            ErrorMessageResourceName = nameof(Resources.Messages.Validations.MaxLength))]
+        public string? LastName { get; set; }
+        // **********
+
+        // **********
+        [System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.BirthDate),
+            ResourceType = typeof(Resources.DataDictionary))]
+        public string? PersianBirthDate { get; set; }
+        // **********
+        // **********
+        [System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.BirthDate),
+            ResourceType = typeof(Resources.DataDictionary))]
+        public System.DateTime? EnglishBirthDate { get; set; }
+        // **********
+
+        // **********
+        [System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.NationalCode),
+            ResourceType = typeof(Resources.DataDictionary))]
+
+        [System.ComponentModel.DataAnnotations.MaxLength(
+            length: Domain.SeedWork.Constant.MaxLength.NationalCode,
+            ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+            ErrorMessageResourceName = nameof(Resources.Messages.Validations.MaxLength))]
+
+        [System.ComponentModel.DataAnnotations.RegularExpression(
+            pattern: Domain.SeedWork.Constant.RegularExpression.NationalCode,
+            ErrorMessageResourceType = typeof(Resources.Messages.Validations),
+            ErrorMessageResourceName = nameof(Resources.Messages.Validations.NationalCode))]
+        public string? NationalCode { get; set; }
+        // **********
+
+        // **********
+        [System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.Description),
+            ResourceType = typeof(Resources.DataDictionary))]
+
+        [System.ComponentModel.DataAnnotations.DataType(
+            dataType: System.ComponentModel.DataAnnotations.DataType.MultilineText)]
+        public string? Description { get; set; }
+        // **********
+
+    }
+}
