02881e38400d84d00cc399b43fc0488043eb3889
Nima Aghaei <88058268+nimaMamAghaei@users.noreply.github.com>
2022-07-06 15:25:17 +0430

    Set Display Name for Gender and Add Enum Extension

--- Domain/Cms/Account/Enumerations/Gender.cs
+++ Domain/Cms/Account/Enumerations/Gender.cs
@@ -2,8 +2,19 @@
 {
 	public enum Gender : int
 	{
+		[System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.Unspecified),
+            ResourceType = typeof(Resources.DataDictionary))]
 		Unspecified = 0,
+
+        [System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.Male),
+            ResourceType = typeof(Resources.DataDictionary))]
 		Male = 1,
+
+        [System.ComponentModel.DataAnnotations.Display(
+            Name = nameof(Resources.DataDictionary.Female),
+            ResourceType = typeof(Resources.DataDictionary))]
 		Female = 2,
 	}
 }
---
+++ Server/Infrastructure/Extensions/EnumExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Infrastructure.Extensions
+{
+    public static class EnumExtension
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            return enumValue.GetType()
+                .GetMember(enumValue.ToString())
+                .First()
+                .GetCustomAttribute<System.ComponentModel.DataAnnotations.DisplayAttribute>()
+                .GetName();
+        }
+    }
+}
